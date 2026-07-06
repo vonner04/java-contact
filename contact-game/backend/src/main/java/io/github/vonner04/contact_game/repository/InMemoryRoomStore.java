794d473b8d91bd9b680a555a4f184e4e774b09bb
vonner04 <randyleean092m@gmail.com>
2026-07-07 07:15:44 +1200

    feat: implementation to track active rooms.
    is this premature optimisation? IDK don't care

---
+++ contact-game/backend/src/main/java/io/github/vonner04/contact_game/repository/InMemoryRoomStore.java
@@ -0,0 +1,50 @@
+package io.github.vonner04.contact_game.repository;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+import io.github.vonner04.contact_game.domain.Room;
+
+public class InMemoryRoomStore implements RoomStore {
+
+    private final Map<String, Room> activeRooms = new ConcurrentHashMap<>();
+
+    @Override
+    public void save(Room item) {
+        activeRooms.put(item.getId(), item);
+    }
+
+    @Override
+    public Optional<Room> findById(String id) {
+        return Optional.ofNullable(activeRooms.get(id));
+    }
+
+    @Override
+    public void deleteById(String id) {
+        activeRooms.remove(id);
+    }
+
+    @Override
+    public boolean existsById(String id) {
+        return activeRooms.containsKey(id);
+    }
+
+    @Override
+    public Collection<Room> findAll() {
+       return activeRooms.values();
+    }
+    
+    @Override
+    public Collection<Room> findExpiredRooms() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findExpiredRooms'");
+    }
+
+    @Override
+    public Optional<Room> findRoomByHostPlayerID(String hostPlayerID) {
+        return activeRooms.values().stream().filter(room -> room.getHostPlayerId().equals(hostPlayerID)).findFirst();
+    }
+    
+}
