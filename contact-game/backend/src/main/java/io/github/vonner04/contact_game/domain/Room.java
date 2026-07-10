aec9de45e1ec5fea71633945eecb0970dcba9b2a
vonner04 <randyleean092m@gmail.com>
2026-07-10 13:06:25 +1200

    feat: new addPlayers() method to append to existing player list

--- contact-game/backend/src/main/java/io/github/vonner04/contact_game/domain/Room.java
+++ contact-game/backend/src/main/java/io/github/vonner04/contact_game/domain/Room.java
@@ -11,54 +11,71 @@ public class Room {
     private String gameMasterId;
     private RoomSettings settings;
     private RoomState state;
-    
+
     public Room(String id, String roomCode, String hostPlayerId, List<Player> players, RoomSettings settings) {
         this.id = id;
-        this. roomCode = roomCode;
+        this.roomCode = roomCode;
         this.hostPlayerId = hostPlayerId;
         this.players = players;
         this.settings = settings;
     }
-    
+
     public String getId() {
         return id;
     }
+
     public String getRoomCode() {
         return roomCode;
     }
+
     public Player getHost() {
         return host;
     }
+
     public void setHost(Player host) {
         this.host = host;
     }
+
     public List<Player> getPlayers() {
         return players;
     }
+
     public void setPlayers(List<Player> players) {
         this.players = players;
     }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
     public String getHostPlayerId() {
         return hostPlayerId;
     }
+
     public void setHostPlayerId(String hostPlayerId) {
         this.hostPlayerId = hostPlayerId;
     }
+
     public String getGameMasterId() {
         return gameMasterId;
     }
+
     public void setGameMasterId(String gameMasterId) {
         this.gameMasterId = gameMasterId;
     }
+
     public RoomSettings getSettings() {
         return settings;
     }
+
     public void setSettings(RoomSettings settings) {
         this.settings = settings;
     }
+
     public RoomState getState() {
         return state;
     }
+
     public void setState(RoomState state) {
         this.state = state;
     }
